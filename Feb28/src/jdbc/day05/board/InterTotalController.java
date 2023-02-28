762339cd39a9d0e2161d51bcee664b47ed8004ce
MIN-DONGHYUN <mdh2057@naver.com>
2023-02-28 17:42:39 +0900

    2023.02.28 ViewContents, Comment, update, delete

---
+++ Feb28/src/jdbc/day05/board/InterTotalController.java
@@ -0,0 +1,10 @@
+package jdbc.day05.board;
+
+import java.util.Scanner;
+
+public interface InterTotalController {
+
+	// 시작 메뉴 TotalController 에 override 재정의 해야함
+	void menu_Start(Scanner sc);
+
+}
